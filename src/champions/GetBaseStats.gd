5309afabc6a126cb078dc54da9bb8ec19e47a901
mgrimsl <xmikegrim@gmail.com>
2023-08-31 22:59:36 -0400

    changing port

--- src/champions/GetBaseStats.gd
+++ src/champions/GetBaseStats.gd
@@ -4,7 +4,7 @@ extends HTTPRequest
 # Called when the node enters the scene tree for the first time.
 func getChampion(id):
 	request_completed.connect(_on_request_completed)
-	request("http://127.0.0.1:8090/api/collections/champions/records/" + id)
+	request("http://20.115.123.255:8081/api/collections/champions/records/" + id)
 
 func _on_request_completed(result, response_code, headers, body):
 	var json = JSON.parse_string(body.get_string_from_utf8())
